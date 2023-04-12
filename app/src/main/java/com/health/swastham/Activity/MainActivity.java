27a2132c1779b214856914539103187d8fde3649
PM <magadumprathamesh9604@gmail.com>
2023-04-12 14:21:35 +0530

    final

--- app/src/main/java/com/health/swastham/Activity/MainActivity.java
+++ app/src/main/java/com/health/swastham/Activity/MainActivity.java
@@ -1,16 +1,98 @@
 package com.health.swastham.Activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
+import com.health.swastham.Activity.Navigation.CameraFragment;
+import com.health.swastham.Activity.Navigation.MessageFragment;
+import com.health.swastham.Activity.Navigation.UserFragment;
+import com.health.swastham.Activity.Navigation.sendActivity;
+import com.health.swastham.Activity.Navigation.shareActivity;
 import com.health.swastham.R;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout=findViewById(R.id.nav_view);
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        NavigationView navigationView=findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment());
+
+
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_draw_open,R.string.navigation_draw_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        if(savedInstanceState==null)
+        {
+            //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+            //navigationView.setCheckedItem(R.id.home);
+
+        }
+
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch(menuItem.getItemId()) {
+
+           // case R.id.home:
+           //     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+           //     break;
+            case R.id.user:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new UserFragment()).commit();
+                break;
+            case R.id.camera:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CameraFragment()).commit();
+                break;
+            case R.id.message:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MessageFragment()).commit();
+                break;
+
+            case R.id.send:
+                Intent intent = new Intent(this, sendActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.share:
+                Intent intent1 = new Intent(this, shareActivity.class);
+                startActivity(intent1);
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer((GravityCompat.START));
+        }
+        else {
+            super.onBackPressed();
+        }
+
+
+    }
+
 }
---
+++ app/src/main/java/com/health/swastham/Activity/Navigation/sendActivity.java
@@ -0,0 +1,16 @@
+package com.health.swastham.Activity.Navigation;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.health.swastham.R;
+
+public class sendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send);
+    }
+}
